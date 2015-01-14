5b43194de73c0c04e4dc8dddda26f7d5a6240589
esteiner <edwin.steiner@inventage.com>
2015-01-14 21:51:21 +0100

    Case für InventageERP hinzugefügt

---
+++ inventageerp.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'inventageerp' do
+  version '1.0.0'
+  sha256 '7cc6b84352f21339694b7a09c6f0a12a4ae5269bee556bdb71d0326acde81013'
+
+  url 'file:///Users/Sonni/Downloads/launchpad.repository-1.0.0-20150113.090254-137-macosx.cocoa.x86_64.zip'
+  name 'InventageERP'
+  homepage 'http://www.inventage.com'
+  license :closed
+
+  app 'InventageERP.app'
+end
