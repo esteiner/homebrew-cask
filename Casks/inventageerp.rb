e17396a3b9fcc4dc82ba83a13b721cc8afb8fdbd
Edwin Steiner <edwin.steiner@inventage.com>
2015-01-14 23:35:44 +0100

    SHA korrigiert

--- Casks/inventageerp.rb
+++ Casks/inventageerp.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'inventageerp' do
   version '1.0.0'
-  sha256 '7cc6b84352f21339694b7a09c6f0a12a4ae5269bee556bdb71d0326acde81013'
+  sha256 '0654529fe8e5d5172f4653cac934d7c2a56c1e4aecf79382028a914b7fef5cd9'
 
   url 'https://inventagedb.inventage.com:7443/p2/launchpad/macosx/InventageERP_Mac_64.zip'
   name 'InventageERP'
